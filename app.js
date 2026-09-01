38616db2906f07599ad1dd9a62363335d7978317
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-01 22:04:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js deployed through automated pipeline");
+  res.send("Hello from Node.js deployed through automated pipeline - 2nd Run");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
