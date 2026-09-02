d5621d4983a3663b0d55e3879a8dfaca1e1025ed
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-02 19:21:40 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js deployed through automated pipeline - 2nd Run");
+  res.send("Hello from Node.js deployed through azure cicd pipeline");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
